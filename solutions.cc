2e0066cf5beb207fdbd4ee0f2a1fe75e834c2f1a
sparrow <280669537@qq.com>
2018-03-30 09:56:32 +0800

    return indices of two numbers such that they add up to a specific target. run_time=2ms. :pear: sparrowli

--- solutions.cc
+++ solutions.cc
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <unordered_map>
 #include <iostream>
 #include <bitset>
 
@@ -92,12 +93,55 @@ class Solution {
     cout<<"hamming distance = "<<hamming_distance<<endl;
     return hamming_distance;
   }
+
+	/*
+	  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+		You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+		Example:
+		Given nums = [2, 7, 11, 15], target = 9,
+
+		Because nums[0] + nums[1] = 2 + 7 = 9,
+		return [0, 1].
+	*/
+  vector<int> Two_Sum(vector<int>& nums, int target) {
+		vector<int> result;
+    
+		unordered_map<int, int> value_vs_index;
+		int temp = 0;
+		for (int i = 0; i < nums.size(); ++i) {
+			temp = target - nums.at(i);
+			if (value_vs_index.find(temp) == value_vs_index.end()) {
+				value_vs_index[nums[i]] = i;
+			} else {
+				result.push_back(value_vs_index[temp]);
+				result.push_back(i);
+				break;
+			}
+		}
+		return result;
+  }
 };
 
 int main(int argc, char* argv[]) {
   Solution solution;
-  solution.Hamming_Distance(1, 4);
+	//vector<int> nums = {2, 7, 11, 15};
+	vector<int> nums;
+	nums.push_back(11);
+	nums.push_back(2);
+	nums.push_back(7);
+	nums.push_back(15);
+
+  vector<int> index_target = solution.Two_Sum(nums, 17);
+	cout<<"[";
+	for (int i = 0; i < index_target.size(); ++i) {
+		cout<<index_target[i]<<"\t";
+	}
+	cout<<"]"<<endl;
 /*
+  Solution solution;
+  solution.Hamming_Distance(1, 4);
   vector<string> strs;
   
   //strs.push_back("lint");
